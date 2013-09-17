74db06a61762d1c74ffa95c2d146e3bdebaa2ea1
jemian <jemian@4ca16b70-4e1b-0410-88e2-31485261bf7f>
2013-09-17 04:47:21 +0000

    enhance the command set - do not know if this will still fit on the Uno
    git-svn-id: http://svn.jemian.org/svn/regitte/arduino/cmd_response@1953 4ca16b70-4e1b-0410-88e2-31485261bf7f

--- cmd_response.ino
+++ cmd_response.ino
@@ -3,35 +3,14 @@
  * simple command/response access via USB to Arduino's I/O capabilities
  */
 
-/***********************************************************************
- Examples
-  
- 1. Read analog input from pin 0:
-
-    >>> ?ai 0
-    41
- 
- 2. Set digital pin 11 for PWM output:
-
-    >>> !pin 11 1
-    Ok
- 
- 3. Set PWM output pin 11 to 128:
- 
-    >>> !pwm 11 128
-    Ok
- 
- 4. Send a bad command (no space between baseCmd and pin):
- 
-    >>> !pwm11 128
-    ERROR_UNKNOWN_COMMAND:!pwm11 128
-
- ***********************************************************************/
+#define SOFTWARE_VERSION       2
+#define SOFTWARE_ID            "cmd_response"
 
 #define BUFFER_LENGTH          40
 #define COMMAND_LENGTH         16
 #define EOS_TERMINATOR_CHAR    '\n'
 #define USB_BAUD               115200
+
 #define UNDEFINED              -1
 
 #define ADC_MAX_VALUE 1023
@@ -39,24 +18,67 @@
 #define PWM_MAX_VALUE 255
 #define PWM_MIN_VALUE 0
 
-//#define TALKATIVE true
+#define PERIOD_DEFAULT_ms  100
+#define PERIOD_MIN_ms      5
+#define PERIOD_MAX_ms      10000
+
+#define MULTIPLIER_DEFAULT  1000
+#define MULTIPLIER_MIN      1
+#define MULTIPLIER_MAX      1000000
 
 char inputString[BUFFER_LENGTH+1];
 char strPtr;
 boolean stringComplete;
-int pin, argValue;
+int pin;
+long argValue;
 char baseCmd[COMMAND_LENGTH+1];
+
+long period = PERIOD_DEFAULT_ms;
+long multiplier = MULTIPLIER_DEFAULT;
+int ai_watched[NUM_ANALOG_INPUTS];
+long ai_sums[NUM_ANALOG_INPUTS];
+float ai_mean[NUM_ANALOG_INPUTS];
+long count;
+long nextUpdate;
 
 void setup() {
   resetBuffer();
   Serial.begin(115200);
   Serial.print(F("cmd_response started: "));
   Serial.println(freeRam());
+  for (int i = 0; i < NUM_ANALOG_INPUTS; i++) {
+    ai_watched[i] = 0;    // by default, do not monitor
+    ai_sums[i] = 0;
+    ai_mean[i] = 0;
+    count = 0;
+  }
+  nextUpdate = millis();
 }
 
 void loop() {
+  signalGathering();
   readBuffer();
   processCmd();
+}
+
+void signalGathering() {
+  // periodically, average the watched ADC channels
+  if (millis() >= nextUpdate) {
+    for (int i = 0; i < NUM_ANALOG_INPUTS; i++) {
+      if (ai_watched[i] && count > 0) {    // if monitoring
+        ai_mean[i] = float(ai_sums[i]) / count;
+        ai_sums[i] = 0;
+      }
+    }
+    nextUpdate = millis() + period;
+    count = 0;
+  }
+  // this is where the ADC channels are read
+  for (int i = 0; i < NUM_ANALOG_INPUTS; i++) {
+    if (ai_watched[i])    // if monitoring
+      ai_sums[i] = analogRead(i);
+  }
+  count++;
 }
 
 /**
@@ -125,11 +147,25 @@ void resetBuffer() {
 void parseBuffer() {
   dissectCommand(inputString);
   if (strlen(baseCmd)) {
-    if      (0 == strcmp(baseCmd, "?ai"))  readAI(inputString);
-    else if (0 == strcmp(baseCmd, "?bi"))  readBI(inputString);
-    else if (0 == strcmp(baseCmd, "!bo"))  writeBO(inputString);
-    else if (0 == strcmp(baseCmd, "!pwm")) writePWM(inputString);
-    else if (0 == strcmp(baseCmd, "!pin")) setPinMode(inputString);
+    if      (0 == strcmp(baseCmd, "?ai"))      readAI(inputString);
+    else if (0 == strcmp(baseCmd, "?bi"))      readBI(inputString);
+    else if (0 == strcmp(baseCmd, "!bo"))      writeBO(inputString);
+    else if (0 == strcmp(baseCmd, "!pwm"))     writePWM(inputString);
+    else if (0 == strcmp(baseCmd, "!pin"))     setPinMode(inputString);
+    else if (0 == strcmp(baseCmd, "?#ai"))     get_num_ai_channels(inputString);
+    else if (0 == strcmp(baseCmd, "?#bi"))     get_num_bi_channels(inputString);
+    else if (0 == strcmp(baseCmd, "!t"))       set_period(inputString);
+    else if (0 == strcmp(baseCmd, "?t"))       get_period(inputString);
+    else if (0 == strcmp(baseCmd, "?t:min"))   get_period_min(inputString);
+    else if (0 == strcmp(baseCmd, "?t:max"))   get_period_max(inputString);
+    else if (0 == strcmp(baseCmd, "!k"))       set_multiplier(inputString);
+    else if (0 == strcmp(baseCmd, "?k"))       get_multiplier(inputString);
+    else if (0 == strcmp(baseCmd, "?k:min"))   get_multiplier_min(inputString);
+    else if (0 == strcmp(baseCmd, "?k:max"))   get_multiplier_max(inputString);
+    else if (0 == strcmp(baseCmd, "!ai:mean")) watch_ai_channel(inputString);
+    else if (0 == strcmp(baseCmd, "?ai:mean")) readAI_mean(inputString);
+    else if (0 == strcmp(baseCmd, "?v"))       get_software_version(inputString);
+    else if (0 == strcmp(baseCmd, "ID"))       get_software_id(inputString);
     else {
       Serial.print(F("ERROR_UNKNOWN_COMMAND:"));
       Serial.println(inputString);
@@ -174,88 +210,156 @@ void dissectCommand(char *source_string) {
 }
 
 
+void finalizeError(char *in) {
+  Serial.println(in);
+  resetBuffer();
+}
+
 void readAI(char* in) {
-  // TODO: range check the pin first!  NUM_ANALOG_INPUTS
-  // TODO: check 0..NUM_ANALOG_INPUTS
-#ifdef TALKATIVE
-  Serial.print(F("ai "));
-  Serial.print(pin);
-  Serial.print(F(" "));
-#endif
-  Serial.println(analogRead(pin));
+  if (pin < 0 || pin > NUM_ANALOG_INPUTS) {
+    Serial.print(F("ERROR_AI_PIN_NOT_AVAILABLE:"));
+    finalizeError(in);
+  } else {
+    Serial.println(analogRead(pin));
+  }
 }
 
 void readBI(char* in) {
-  // TODO: range check the pin first!  NUM_DIGITAL_PINS
-  // TODO: check 0..NUM_DIGITAL_PINS
-#ifdef TALKATIVE
-  Serial.print(F("bi "));
-  Serial.print(pin);
-  Serial.print(F(" "));
-#endif
-  Serial.println(digitalRead(pin));
+  if (pin < 0 || pin > NUM_DIGITAL_PINS) {
+    Serial.print(F("ERROR_BI_PIN_NOT_AVAILABLE:"));
+    finalizeError(in);
+  } else {
+    Serial.println(digitalRead(pin));
+  }
 }
 
 void writeBO(char* in) {
   if (argValue < 0 || argValue > 1) {
     Serial.print(F("ERROR_BINARY_RANGE:"));
-    Serial.println(inputString);
-    resetBuffer();
+    finalizeError(in);
+  } else if (pin < 0 || pin > NUM_DIGITAL_PINS) {
+    // TODO: but did we set the pin for output?
+    Serial.print(F("ERROR_BO_PIN_NOT_AVAILABLE:"));
+    finalizeError(in);
   } else {
-    // TODO: range check the pin first and make sure it is OK for output!   NUM_DIGITAL_PINS
-	// TODO: check 0..NUM_DIGITAL_PINS
     digitalWrite(pin, argValue);
-#ifdef TALKATIVE
-    Serial.print(F("bo "));
-    Serial.print(pin);
-    Serial.print(F(" "));
-    Serial.println(argValue);
-#else
-  Serial.println(F("Ok"));
-#endif
+    Serial.println(F("Ok"));
   }
 }
 
 void writePWM(char* in) {
   if (argValue < PWM_MIN_VALUE || argValue > PWM_MAX_VALUE) {
     Serial.print(F("ERROR_PWM_RANGE:"));
-    Serial.println(inputString);
-    resetBuffer();
+    finalizeError(in);
+  } else if (!digitalPinHasPWM(pin)) {
+    // TODO: but did we set the pin for output?
+    Serial.print(F("ERROR_PIN_NOT_PWM:"));
+    finalizeError(in);
   } else {
-    // TODO: range check the pin first and make sure it is OK for PWM output!
-	// TODO: check 0..NUM_DIGITAL_PINS
-	if (digitalPinHasPWM(pin)) {
-      analogWrite(pin, argValue);
-#ifdef TALKATIVE
-      Serial.print(F("pwm "));
-      Serial.print(pin);
-      Serial.print(F(" "));
-      Serial.println(argValue);
-#else
-	  Serial.println(F("Ok"));
-#endif
-    } else {
-      Serial.print(F("ERROR_PIN_NOT_PWM:"));
-      Serial.println(inputString);
-      resetBuffer();
-	}
+    analogWrite(pin, argValue);
+    Serial.println(F("Ok"));
   }
 }
 
+void get_num_ai_channels(char* in) {
+  Serial.println(NUM_ANALOG_INPUTS);
+}
+
+void get_num_bi_channels(char* in) {
+  Serial.println(NUM_DIGITAL_PINS);
+}
+
+void set_period(char* in) {
+  if (argValue < PERIOD_MIN_ms || argValue > PERIOD_MAX_ms) {
+    Serial.print(F("ERROR_PERIOD_RANGE:"));
+    finalizeError(in);
+  } else {
+    period = argValue;
+    Serial.println(F("Ok"));
+  }
+}
+
+void get_period(char* in) {
+  Serial.println(period);
+}
+
+void get_period_min(char* in) {
+  Serial.println(PERIOD_MIN_ms);
+}
+
+void get_period_max(char* in) {
+  Serial.println(PERIOD_MAX_ms);
+}
+
+void set_multiplier(char* in) {
+  if (argValue < MULTIPLIER_MIN || argValue > MULTIPLIER_MAX) {
+    Serial.print(F("ERROR_MULTIPLIER_RANGE:"));
+    finalizeError(in);
+  } else {
+    multiplier = argValue;
+    Serial.println(F("Ok"));
+  }
+}
+
+void get_multiplier(char* in) {
+  Serial.println(multiplier);
+}
+
+void get_multiplier_min(char* in) {
+  Serial.println(MULTIPLIER_MIN);
+}
+
+void get_multiplier_max(char* in) {
+  Serial.println(MULTIPLIER_MAX);
+}
+
+void get_software_version(char* in) {
+  Serial.println(SOFTWARE_VERSION);
+}
+
+void get_software_id(char* in) {
+  // this is the ONLY string output that is not an error
+  Serial.println(F(SOFTWARE_ID));
+}
+
+void watch_ai_channel(char* in) {
+  // configure pin to be averaged (or not depending on argValue)
+  if (pin < 0 || pin > NUM_ANALOG_INPUTS) {
+    Serial.print(F("ERROR_AI_PIN_NOT_AVAILABLE:"));
+    finalizeError(in);
+  } else {
+    ai_watched[pin] = (argValue == 0 ? 0 : 1);
+    Serial.println(F("Ok"));
+  }
+}
+
+void readAI_mean(char* in) {
+  if (pin < 0 || pin > NUM_ANALOG_INPUTS) {
+    Serial.print(F("ERROR_AI_PIN_NOT_AVAILABLE:"));
+    finalizeError(in);
+  } else if (!ai_watched[pin]) {
+    Serial.print(F("ERROR_AI_PIN_NOT_WATCHED:"));
+    finalizeError(in);
+  } else {
+    Serial.println(long(ai_mean[pin] * multiplier));
+  }
+}
+
+void not_implemented_yet(char* in) {
+  Serial.print(F("ERROR_NOT_IMPLEMENTED_YET:"));
+  finalizeError(in);
+}
+
 void setPinMode(char* in) {
-  // TODO: check 0..NUM_DIGITAL_PINS
   argValue = (argValue == 1) ? OUTPUT : INPUT;
-  pinMode(pin, argValue);
-    // TODO: range check the pin first and make sure it is OK for output!   NUM_DIGITAL_PINS
-	// TODO: check 0..NUM_DIGITAL_PINS
-#ifdef TALKATIVE
-  Serial.print(F("pin "));
-  Serial.print(pin);
-  Serial.print(F(" "));
-  Serial.println(argValue);
-#else
-  Serial.println(F("Ok"));
-#endif
+  if (pin < 0 || pin > NUM_DIGITAL_PINS) {
+    // TODO: but did we set the pin for output?
+    Serial.print(F("ERROR_DIGITAL_PIN_NOT_AVAILABLE:"));
+    finalizeError(in);
+  } else {
+    pinMode(pin, argValue);
+    Serial.println(F("Ok"));
+  }
 }
 
 //########## SVN repository information ###################
--- cmd_response.rst
+++ cmd_response.rst
@@ -25,14 +25,54 @@ Arduino's I/O capabilities.
 Arduino Interface
 ====================
 
+short introduction here
+
+Theory of Operations
++++++++++++++++++++++
+
+.. TODO:
+
+basic reads and writes are very short
+
+ADC signal averaging requires some more setup
+
+averaging all signals can make unit very slow
+
+optimize by watching only the channels to be averaged
+
+averaging occurs after each fixed-time period
+
+user can query and adjust the time period
+
+the time period can only be within a fixed range,
+the user can query the min and max of this range
+
+user can start or stop watching a channel for averaging
+
+reporting of average ADC value should be an integer 
+(so EPICS can use this as the RVAL)
+
+user can query and adjust a scaling factor (multiplier), as in: 
+multiplier * sum(ADC)/count
+
+the multiplier can only be within a fixed range,
+the user can query the min and max of this range
+
+an ID and a version number are available to be read
+
+Tables
++++++++++
+
 .. rubric:: Table: Communications Parameters
 
-====================   ========
-term                   value
-====================   ========
-communications rate    115200
-line terminator        ``\n``
-====================   ========
+======================   ========
+term                     value
+======================   ========
+communications rate      115200
+line terminator          ``\n``
+buffer length (chars)    40
+command length (chars)   16
+======================   ========
 
 .. rubric:: Table: USB command interface
 
@@ -46,24 +86,38 @@ command           value     action
 ``!bo pin v``     0..1      sets numbered digital output to value v
 ``!pwm pin v``    0..255    sets numbered PWM digital output to value v
 ``!pin pin v``    0..1      sets mode of digital pin to value v (value: 1=OUTPUT, not 1=INPUT)
+``?#ai``	  int	    returns NUM_ANALOG_INPUTS
+``?#bi``	  int 	    returns NUM_DIGITAL_PINS
+``!t``		  long 	    sets averaging time, ms
+``?t``		  long 	    returns averaging time, ms
+``?t:min``	  long 	    returns minimum allowed averaging time, ms
+``?t:max``	  long 	    returns maximum allowed averaging time, ms
+``!k``		  long 	    sets averaging factor (``k``)
+``?k``		  long 	    returns averaging factor (``k``)
+``?k:min``	  long 	    returns minimum allowed averaging factor (``k``)
+``?k:max``	  long 	    returns maximum allowed averaging factor (``k``)
+``!ai:mean``	  0..1 	    sets up ai pin for averaging
+``?ai:mean``	  long 	    returns ``<ai>*k``
+``?v``		  long 	    returns version number
+``ID``		  0 	    returns identification string
 other             ..        returns "ERROR_UNKNOWN_COMMAND:text"
 ================  ========  =====================================================================
 
 .. possible new commands
-   ``?#ai``      returns   NUM_ANALOG_INPUTS
-   ``?#bi``      returns   NUM_DIGITAL_PINS
-   ``!t``        sets      averaging time, ms
-   ``?t``        returns   averaging time, ms
-   ``?t:min``    returns   minimum allowed averaging time, ms
-   ``?t:max``    returns   maximum allowed averaging time, ms
-   ``!k``        sets      averaging factor (k)
-   ``?k``        returns   averaging factor (k)
-   ``?k:min``    returns   minimum allowed averaging factor (k)
-   ``?k:max``    returns   maximum allowed averaging factor (k)
-   ``!ai:mean``  sets      Sets up ai pin for averaging
-   ``?ai:mean``  returns   <ai>*k
-   ``?v``        returns   version number
-   ``ID``        returns   identification string
+   ``?#ai``      returns NUM_ANALOG_INPUTS
+   ``?#bi``      returns NUM_DIGITAL_PINS
+   ``!t``        sets averaging time, ms
+   ``?t``        returns averaging time, ms
+   ``?t:min``    returns minimum allowed averaging time, ms
+   ``?t:max``    returns maximum allowed averaging time, ms
+   ``!k``        sets averaging factor (``k``)
+   ``?k``        returns averaging factor (``k``)
+   ``?k:min``    returns minimum allowed averaging factor (``k``)
+   ``?k:max``    returns maximum allowed averaging factor (``k``)
+   ``!ai:mean``  sets up ai pin for averaging
+   ``?ai:mean``  returns ``<ai>*k``
+   ``?v``        returns version number
+   ``ID``        returns identification string
 
 notes: 
 
@@ -73,6 +127,9 @@ notes:
 #. "?" commands return an integer
 #. "!" commands return "Ok"
 #. Errors, starting with "ERROR_" will substitute for expected output
+
+Commands
++++++++++++
 
 ?ai command
 ----------------
@@ -84,9 +141,7 @@ notes:
 Reads the analog input *pin* specified in the first argument.
 The returned value is between 0 and 1023 (10-bit ADC) which represents
 a measured voltage between 0 and 5 VDC.
-
-.. note:: pin number is not checked but will be 
-   in a future version (``0..NUM_ANALOG_INPUTS``)
+The pin must be within the range of ``0..NUM_ANALOG_INPUTS``
 
 ?bi command
 ----------------
@@ -97,9 +152,7 @@ a measured voltage between 0 and 5 VDC.
 
 Reads the digital input *pin* specified in the first argument.
 The returned value is either 0 or 1.
-
-.. note:: pin number is not checked but will be 
-   in a future version (``0..NUM_DIGITAL_PINS``)
+The pin must be within the range of ``0..NUM_DIGITAL_PINS``.
 
 !bo command
 ----------------
@@ -111,10 +164,8 @@ The returned value is either 0 or 1.
 Sets the digital output *pin* specified in the first argument 
 to the *value* specified in the second argument.  If the syntax
 is correct and the value is within range, returns ``Ok``.  
-The pin must first be configured for output by calling ``!pin pin 1``.
-
-.. note:: pin number is not checked but will be 
-   in a future version (``0..NUM_DIGITAL_PINS``)
+The pin must be within the range of ``0..NUM_DIGITAL_PINS``
+and must first be configured for output by calling ``!pin pin 1``.
 
 !pwm command
 ----------------
@@ -150,11 +201,24 @@ value   meaning
 1       use pin as output
 ======  =================
 
-.. note:: pin number is not checked but will be in a 
-   future version (``0..NUM_DIGITAL_PINS``)
+.. document each of these commands
+   ?#ai	     int       returns NUM_ANALOG_INPUTS
+   ?#bi	     int       returns NUM_DIGITAL_PINS
+   !t	     long      sets averaging time, ms
+   ?t	     long      returns averaging time, ms
+   ?t:min    long      returns minimum allowed averaging time, ms
+   ?t:max    long      returns maximum allowed averaging time, ms
+   !k	     long      sets averaging factor (``k``)
+   ?k	     long      returns averaging factor (``k``)
+   ?k:min    long      returns minimum allowed averaging factor (``k``)
+   ?k:max    long      returns maximum allowed averaging factor (``k``)
+   !ai:mean  0..1      sets up ai pin for averaging
+   ?ai:mean  long      returns ``<ai>*k``
+   ?v	     long      returns version number
+   ID	     0         returns identification string
 
 Examples
---------------------
++++++++++++
 
 1. Read analog input from pin 0:
 
@@ -178,7 +242,7 @@ ERROR_UNKNOWN_COMMAND:!pwm11 128
 
 
 Error messages
---------------------
+++++++++++++++++++++++
 
 This is a list of the possible error messages and their meanings.
 All error messages begin with the text ``ERROR_`` and then some 
@@ -186,8 +250,24 @@ terse descriptor of the error.
 In most cases, the input that triggered the error message is 
 returned.  A single ":" is used as the delimiter when the input is appended.
 
+``ERROR_AI_PIN_NOT_AVAILABLE:input``
+   The specified pin number is not available for 
+   analog input on this hardware.
+
+``ERROR_AI_PIN_NOT_WATCHED:input``
+   The specified pin number was not set up for averaging.
+   Need to call ``!ai:mean pin 1`` to enable averaging on this pin.
+
 ``ERROR_BINARY_RANGE:input``
   The value must be either ``0`` or ``1``.
+
+``ERROR_BI_PIN_NOT_AVAILABLE:input``
+   The specified pin number is not available for 
+   binary (digital) input on this hardware.
+
+``ERROR_BO_PIN_NOT_AVAILABLE:input``
+   The specified pin number is not available for 
+   binary (digital) output on this hardware.
 
 ``ERROR_BUFFER_OVERFLOW``
   Too many characters were received before the line terminator.
@@ -201,20 +281,22 @@ returned.  A single ":" is used as the delimiter when the input is appended.
   the pin number.  This command is generated when no space is detected
   in the input.
 
+``ERROR_DIGITAL_PIN_NOT_AVAILABLE:input``
+  The specified binary (digital) pin number is 
+  not available on this hardware.
+
 ``ERROR_PIN_NOT_PWM:input``
   The specified pin is not supported for PWM on this Arduino hardware.
   This is determined by calling the Arduino system macro 
   ``digitalPinHasPWM(pin)`` which is defined for each different type 
   of Arduino hardware variation.
 
+``ERROR_NOT_IMPLEMENTED_YET:input``
+  The command specified has not yet been implemented.
+
 ``ERROR_PWM_RANGE:input``
   The PWM value must be between 0 and 255, inclusive.
   This error is reported for any value outside this range.
-
-``ERROR_UNKNOWN_COMMAND:input``
-  The input was not recognized as a valid command.
-  One reason for this might be the use of upper case.
-  Other possibilities exist.
 
 ``ERROR_TOO_MANY_ARGUMENTS:input``
   The general form for input commands is ``baseCmd pin [value]``
@@ -224,7 +306,12 @@ returned.  A single ":" is used as the delimiter when the input is appended.
   
   At this time, if ``value`` is specified for a "?" (read) command,
   it is ignored.  In the future, this will generate an error message.
-  
+
+``ERROR_UNKNOWN_COMMAND:input``
+  The input was not recognized as a valid command.
+  One reason for this might be the use of upper case.
+  Other possibilities exist.
+
 ..
 	EPICS Streams protocol
 	=======================
